44b347a16a8b039f23bb8646905cf36dd537359c
Nathan219 <Nathan219@gmail.com>
2014-07-08 15:18:38 -0700

    Now detects if values have a $ at the start, and (if it does), escapes it

--- configToEnvFiles.py
+++ configToEnvFiles.py
@@ -1,12 +1,18 @@
 import sys
 import string
 import re
-def safeword(input, toUpper):
-    variable = input.strip(' \t\n\r,"')
-    if toUpper:
-        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', variable)
-        variable = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).upper()
+def safeVariable(input):
+    variable = input.strip(' \t\n\r,"').replace(" ", "")
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', variable)
+    variable = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).upper()
     return re.sub(r"[^\w\d]", "_", variable)
+
+def safeword(input):
+    variable = input.strip(' \t\n\r,')
+    replacement = "\$"
+    if re.search(r'^(")', variable):
+        replacement = '"' + replacement
+    return re.sub(r'^(")?\$', replacement, variable)
 
 def parse(inputfile, outputfile, parentprefix):
     line = inputfile.readline()
@@ -14,7 +20,7 @@ def parse(inputfile, outputfile, parentprefix):
         return False
     elif "{" in line and '}' not in line:
         splitparts = string.split(line, ":", 1)
-        variable = safeword(splitparts[0], True)
+        variable = safeVariable(splitparts[0])
         parentprefix += variable + "_"
         condition = True
         while condition:
@@ -22,19 +28,19 @@ def parse(inputfile, outputfile, parentprefix):
         return True
     elif "[" in line and "]" not in line:
         splitparts = string.split(line, ":", 1)
-        variable = safeword(splitparts[0], True)
+        variable = safeVariable(splitparts[0])
         line = inputfile.readline()
-        outputfile.write(parentprefix + variable + "=[" + line.strip(' \t\n\r,'))
+        outputfile.write(parentprefix + variable + "=[" + safeword(line))
         line = inputfile.readline()
         while "]" not in line:
-            outputfile.write("," + line.strip(' \t\n\r,'))
+            outputfile.write("," + safeword(line))
             line = inputfile.readline()
         outputfile.write("]\n")
         return True
     elif ":" in line:
         splitparts = string.split(line, ":", 1)
-        variable = parentprefix + safeword(splitparts[0], True)
-        towrite = "{}={}".format(variable, splitparts[1].strip(' \t\n\r,'))
+        variable = parentprefix + safeVariable(splitparts[0])
+        towrite = "{}={}".format(variable, safeword(splitparts[1]))
         print(towrite)
         outputfile.write(towrite + "\n")
         return True
